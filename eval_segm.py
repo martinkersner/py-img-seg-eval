173560b22492214b5817462c17e97ef09f458441
Martin Keršner <m.kersner@gmail.com>
2015-11-30 19:41:20 +0900

    the first version of evaluation of segmentation algorithms

---
+++ eval_segm.py
@@ -0,0 +1,161 @@
+#!/usr/bin/python
+
+'''
+Martin Kersner, m.kersner@gmail.com
+2015/11/30
+
+Evaluation metrics for image segmentation inspired by
+paper Fully Convolutional Networks for Semantic Segmentation.
+
+TODO frequency weighted IU
+'''
+
+import numpy as np
+
+def pixel_accuracy(eval_segm, gt_segm):
+    '''
+    sum_i(n_ii) / sum_i(t_i)
+    pixel_accuracy() does not consider classes which are not included in ground 
+    truth segmentation.
+    '''
+
+    check_size(eval_segm, gt_segm)
+
+    cl, n_cl = extract_classes(gt_segm)
+    eval_mask, gt_mask = extract_both_masks(eval_segm, gt_segm, cl, n_cl)
+
+    sum_n_ii = 0
+    sum_t_i  = 0
+
+    for i, c in enumerate(cl):
+        curr_eval_mask = eval_mask[i, :, :]
+        curr_gt_mask = gt_mask[i, :, :]
+
+        sum_n_ii += np.sum(np.logical_and(curr_eval_mask, curr_gt_mask))
+        sum_t_i  += np.sum(curr_gt_mask)
+ 
+    if (sum_t_i == 0):
+        pixel_accuracy_ = 0
+    else:
+        pixel_accuracy_ = sum_n_ii / sum_t_i
+
+    return pixel_accuracy_
+
+def mean_accuracy():
+    '''
+    (1/n_cl) sum_i(n_ii/t_i)
+    '''
+
+    check_size(eval_segm, gt_segm)
+
+    cl, n_cl = extract_classes(gt_segm)
+    eval_mask, gt_mask = extract_both_masks(eval_segm, gt_segm, cl, n_cl)
+
+    accuracy = list([0]) * n_cl
+
+    for i, c in enumerate(cl):
+        curr_eval_mask = eval_mask[i, :, :]
+        curr_gt_mask = gt_mask[i, :, :]
+
+        n_ii = np.sum(np.logical_and(curr_eval_mask, curr_gt_mask))
+        t_i  = np.sum(curr_gt_mask)
+ 
+    if (t_i != 0):
+        accuracy[i] = n_ii / t_i
+
+    mean_accuracy_ = np.mean(accuracy)
+    return mean_accuracy_
+
+def mean_IU(eval_segm, gt_segm):
+    '''
+    (1/n_cl) * sum_i(n_ii / (t_i + sum_j(n_ji) - n_ii))
+    Superfluous class in segmentation is treated the same way as not found class.
+    '''
+
+    check_size(eval_segm, gt_segm)
+
+    cl, n_cl = union_classes(eval_segm, gt_segm)
+    eval_mask, gt_mask = extract_both_masks(eval_segm, gt_segm, cl, n_cl)
+
+    IU = list([0]) * n_cl
+
+    for i, c in enumerate(cl):
+        curr_eval_mask = eval_mask[i, :, :]
+        curr_gt_mask = gt_mask[i, :, :]
+ 
+        if (np.sum(curr_eval_mask) == 0) or (np.sum(curr_gt_mask) == 0):
+            continue
+
+        n_ii = np.sum(np.logical_and(curr_eval_mask, curr_gt_mask))
+        t_i  = np.sum(curr_gt_mask)
+        n_ij = np.sum(curr_eval_mask)
+
+        IU[i] = n_ii / (t_i + n_ij - n_ii)
+ 
+    mean_IU_ = np.mean(IU)
+    return mean_IU_
+
+def frequency_weighted_UI(eval_segm, gt_segm):
+    '''
+    sum_k(t_k)^(-1) * sum_i((t_i*n_ii)/(t_i + sum_j(n_ji) - n_ii))
+    '''
+    pass
+
+'''
+Auxiliary functions used during evaluation.
+'''
+def extract_both_masks(eval_segm, gt_segm, cl, n_cl):
+    eval_mask = extract_masks(eval_segm, cl, n_cl)
+    gt_mask   = extract_masks(gt_segm, cl, n_cl)
+
+    return eval_mask, gt_mask
+
+def extract_classes(segm):
+    cl = np.unique(segm)
+    n_cl = len(cl)
+
+    return cl, n_cl
+
+def union_classes(eval_segm, gt_segm):
+    eval_cl, _ = extract_classes(eval_segm)
+    gt_cl, _   = extract_classes(gt_segm)
+
+    cl = np.union1d(eval_cl, gt_cl)
+    n_cl = len(cl)
+
+    return cl, n_cl
+
+def extract_masks(segm, cl, n_cl):
+    h, w  = segm_size(segm)
+    masks = np.zeros((n_cl, h, w))
+
+    for i, c in enumerate(cl):
+        masks[i, :, :] = segm == c
+
+    return masks
+
+def segm_size(segm):
+    try:
+        height = segm.shape[0]
+        width  = segm.shape[1]
+    except IndexError:
+        raise
+
+    return height, width
+
+def check_size(eval_segm, gt_segm):
+    h_e, w_e = segm_size(eval_segm)
+    h_g, w_g = segm_size(gt_segm)
+
+    if (h_e != h_g) or (w_e != w_g):
+        raise EvalSegErr("DiffDim: Different dimensions of matrices!")
+
+'''
+Exceptions
+'''
+class EvalSegErr(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
